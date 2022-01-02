5c1cff838e48c1cd69b3e67969e7fc9e4efec480
shareefholley <64617365+shareefholley@users.noreply.github.com>
2022-01-01 22:28:06 -0800

    Add files via upload

---
+++ rippel.js
@@ -0,0 +1,11 @@
+
+
+
+function myFunction() {
+    var myLinks = document.getElementById("myLinks");
+  if (myLinks.style.display === "block") {
+    myLinks.style.display = "none";
+  } else {
+    myLinks.style.display = "block";
+  }
+};
